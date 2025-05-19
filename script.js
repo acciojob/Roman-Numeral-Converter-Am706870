1cc2317930aa0a08351a1524118fa16a9df30f59
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-05-19 11:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,27 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	
+  let res = "";
+
+  // Iterate using numeric keys in order
+  for (let i = 0; i <= 6; i++) {
+    const [symbol, value] = obj[i];
+    while (num >= value) {
+      res += symbol;
+      num -= value;
+    }
+  }
+
+  return res;
+}
 
   //your code here
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
